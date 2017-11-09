9748740c85d821b3fb09bc533ddd4fe7f5f24e2e
Ethan B <e10benga@gmail.com>
2017-11-09 17:26:20 -0600

    Finished dialogue box commands and testers

--- Unity/Assets/Scripts/DialogueController.cs
+++ Unity/Assets/Scripts/DialogueController.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 public class DialogueController : MonoBehaviour {
 	public Text speakerText;
 	public Text messageText;
+	public Image icon;
 	public GameObject dialogueBox;
 
 	private bool active = false;
+	private bool done;
 
 	public bool Active {
 		get {
@@ -16,6 +18,49 @@ public class DialogueController : MonoBehaviour {
 		}
 		set {
 			active = value;
+		}
+	}
+
+	public void speak(string message, string speaker) {
+		done = false;
+		speakerText.text = speaker;
+		messageText.text = "";
+		dialogueBox.SetActive (true);
+
+		StartCoroutine (speak (message.ToCharArray ()));
+	}
+
+	public void speak(string message, string speaker, Sprite s) {
+		done = false;
+		speakerText.text = speaker;
+		messageText.text = "";
+		dialogueBox.SetActive (true);
+		icon.sprite = s;
+
+		StartCoroutine (speak (message.ToCharArray ()));
+	}
+
+	IEnumerator speak(char[] message) {
+		active = true;
+		bool skip = false;
+		foreach (char c in message) {
+			if (!skip) {
+				yield return new WaitForSeconds (0.02f);
+			}
+			messageText.text += c;
+			if (Input.GetMouseButtonDown (1)) {
+				skip = true;
+			}
+		}
+		messageText.text += "\nPress any key to continue...";
+		done = true;
+	}
+
+	void Update() {
+		if (done && Input.anyKeyDown) {
+			active = false;
+			dialogueBox.SetActive (false);
+			icon.sprite = null;
 		}
 	}
 }
---
+++ Unity/Assets/Scripts/TESTING/DialogueTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTest : MonoBehaviour {
+	public InputField input;
+	DialogueController d;
+
+	void Start() {
+		d = FindObjectOfType<DialogueController> ();
+	}
+
+	public void updateText() {
+		if (!d.Active)
+			d.speak (input.text, "Speaker " + Random.Range (1, 10));
+	}
+}
