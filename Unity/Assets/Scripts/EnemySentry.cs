e68d515acdaf6b51f1f58a1f4746231e5992fac0
Ethan B <e10benga@gmail.com>
2017-11-10 13:48:27 -0600

    In Progress

---
+++ Unity/Assets/Scripts/EnemySentry.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySentry : MonoBehaviour {
+	public float angle1 = -90f;
+	public float angle2 = 90;
+
+	public float speed = .1f;
+
+	private bool left = false;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
