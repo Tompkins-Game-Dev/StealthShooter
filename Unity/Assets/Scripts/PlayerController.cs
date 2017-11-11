591fb6063d48851119313ced9a868f7e55276cce
IJCX48 <hesitantray2@gmail.com>
2017-11-11 16:28:13 -0600

    Added ranged attacks

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
+	public Rigidbody projectile;
+	public KeyCode attack;
+	public float movement_speed, projectile_speed;
 
 	private Camera cam;
 	private Rigidbody2D body;
 
-	void Start() {
+	void Start () {
 		cam = Camera.main;
 		body = GetComponent <Rigidbody2D> ();
 	}
 
-	void Update() {
+	void Update () {
 		Vector3 position_two = cam.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 10));
 		Vector3 movement_vector = new Vector3 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"), 0).normalized;
 		position_two = transform.position - position_two;
@@ -25,6 +27,11 @@ public class PlayerController : MonoBehaviour {
 			transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 180 - (Mathf.Rad2Deg * (Mathf.Asin (position_two.x / position_two.magnitude)))));
 		}
 
-		body.MovePosition (transform.position + new Vector3 (movement_vector.x * speed, movement_vector.y * speed) * Time.deltaTime);
+		body.MovePosition (transform.position + new Vector3 (movement_vector.x * movement_speed, movement_vector.y * movement_speed) * Time.deltaTime);
+
+		if (Input.GetKeyUp (attack)) {
+			Rigidbody new_projectile = (Rigidbody) Instantiate (projectile, transform.position + (transform.up * 2), transform.rotation);
+			new_projectile.AddForce (transform.up * projectile_speed);
+		}
 	}
 }
