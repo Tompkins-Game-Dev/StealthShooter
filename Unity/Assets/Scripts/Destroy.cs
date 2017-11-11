d2e86e93d87f00f5a489ec80448c1c54b01296f8
IJCX48 <hesitantray2@gmail.com>
2017-11-11 16:28:03 -0600

    Added ranged attacks

---
+++ Unity/Assets/Scripts/Destroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public float time;
+
+	// Update is called once per frame
+	void Update () {
+		time -= Time.deltaTime;
+
+		if (time < 0) {
+			Destroy (this.gameObject);
+		}
+	}
+}
